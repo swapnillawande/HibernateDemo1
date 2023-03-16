f46d96aef9c5390e434a389f030f80a018ef4b92
swapnil <swapnillawande2001@gmail.com>
2023-03-16 21:35:30 +0530

    v1

---
+++ src/main/java/com/swappylearn/entities/Employee.java
@@ -0,0 +1,49 @@
+package com.swappylearn.entities;
+
+public class Employee {
+	
+	
+	private int eno;
+	private String name;
+	private String address;
+ 
+	
+	public Employee() {
+		
+	}
+ 
+	public Employee(int eno, String name, String address) {
+		super();
+		this.eno = eno;
+		this.name = name;
+		this.address = address;
+	}
+
+	public int getEno() {
+		return eno;
+	}
+
+	public void setEno(int eno) {
+		this.eno = eno;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	
+
+}
